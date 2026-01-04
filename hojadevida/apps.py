e6944980b965f6b48daa9fcdae883c873252a2f1
Vanessa <vanedelsaltos@gamil.com>
2026-01-04 12:05:46 -0500

    cambiar la parte de apps.py como antes

--- hojadevida/apps.py
+++ hojadevida/apps.py
@@ -1,21 +1,6 @@
 from django.apps import AppConfig
 
-
 class HojadevidaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hojadevida'
 
-    def ready(self):
-        from django.contrib.auth.models import User
-
-        SUPERUSER_USERNAME = 'vane'
-        SUPERUSER_EMAIL = 'vanedelsaltos@gmail.com'
-        SUPERUSER_PASSWORD = 'vane1971'
-
-        if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
-            print("Creando superusuario automáticamente...")
-            User.objects.create_superuser(
-                username=SUPERUSER_USERNAME,
-                email=SUPERUSER_EMAIL,
-                password=SUPERUSER_PASSWORD
-            )
