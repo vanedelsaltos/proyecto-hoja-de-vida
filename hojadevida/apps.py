ebe66c689eb3e944e4596e98d6c88948fac46c03
Vanessa <vanedelsaltos@gamil.com>
2026-01-04 11:41:39 -0500

    Fix superuser creation without django.setuparreglar la parte de apps.py

--- hojadevida/apps.py
+++ hojadevida/apps.py
@@ -1,22 +1,17 @@
 from django.apps import AppConfig
-from django.conf import settings
+
 
 class HojadevidaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hojadevida'
 
     def ready(self):
-        # Este código se ejecuta cuando la app arranca
-        import django
-        django.setup()
         from django.contrib.auth.models import User
 
-        # Cambia estos datos por los que quieras
         SUPERUSER_USERNAME = 'vane'
         SUPERUSER_EMAIL = 'vanedelsaltos@gmail.com'
         SUPERUSER_PASSWORD = 'vane1971'
 
-        # Crear superusuario solo si no existe
         if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
             print("Creando superusuario automáticamente...")
             User.objects.create_superuser(
