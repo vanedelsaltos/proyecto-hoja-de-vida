9515f0864daf6df55a7bc9ed53788b0137ebe41c
Vanessa <vanedelsaltos@gamil.com>
2026-01-03 20:49:44 -0500

    Para Crear superusuario automáticamente al desplegar

--- hojadevida/apps.py
+++ hojadevida/apps.py
@@ -1,5 +1,26 @@
 from django.apps import AppConfig
-
+from django.conf import settings
 
 class HojadevidaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'hojadevida'
+
+    def ready(self):
+        # Este código se ejecuta cuando la app arranca
+        import django
+        django.setup()
+        from django.contrib.auth.models import User
+
+        # Cambia estos datos por los que quieras
+        SUPERUSER_USERNAME = 'admin'
+        SUPERUSER_EMAIL = 'admin@ejemplo.com'
+        SUPERUSER_PASSWORD = 'TuContraseña123'
+
+        # Crear superusuario solo si no existe
+        if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+            print("Creando superusuario automáticamente...")
+            User.objects.create_superuser(
+                username=SUPERUSER_USERNAME,
+                email=SUPERUSER_EMAIL,
+                password=SUPERUSER_PASSWORD
+            )
